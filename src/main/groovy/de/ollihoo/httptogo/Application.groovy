5d997d50d3313a73b71a09b7613cdb26cfb242b0
ollihoo <oliver@hoogvliet.de>
2016-07-02 10:19:13 +0200

    Add changes to switch to undertow

--- src/main/groovy/de/ollihoo/httptogo/Application.groovy
+++ src/main/groovy/de/ollihoo/httptogo/Application.groovy
@@ -1,12 +1,34 @@
 package de.ollihoo.httptogo
 
+import io.undertow.Undertow
+import io.undertow.UndertowOptions
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @ComponentScan("de.ollihoo")
 class Application {
+
+    @Bean
+    UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+        UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory()
+        UndertowBuilderCustomizer customizer = createCustomizerWithHttp2()
+        factory.addBuilderCustomizers(customizer)
+        factory
+    }
+
+    private UndertowBuilderCustomizer createCustomizerWithHttp2() {
+        new UndertowBuilderCustomizer() {
+            @Override
+            void customize(Undertow.Builder builder) {
+                builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true)
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args)
